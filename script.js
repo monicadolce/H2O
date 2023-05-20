cb1df082774563b3257f55770396497a1a3374a0
Monica <monicadolcemascolo@gmail.com>
2023-05-20 17:13:53 -0400

    fixed error in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const smallCups = document.querySelectorAll('.cup-small')
-const liters = document.getElementBy('liters')
+const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
