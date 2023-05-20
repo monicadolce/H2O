45137fd1938d22705f7756b614a08e8170ec9485
Monica <monicadolcemascolo@gmail.com>
2023-05-20 16:37:19 -0400

    working on script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementBy('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
